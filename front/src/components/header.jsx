38f97964c81c58958964ccd8b65ce45618ef9d95
squushe bc2025-5 <maksimrozko49@gmail.com>
2025-11-13 18:20:50 +0200

    Added links to header and Login button

--- front/src/components/header.jsx
+++ front/src/components/header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <nav className="bg-gray-900 border-b border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="#"
+          <Link
+            to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
               src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8" // "class" замінено на "className"
+              className="h-8"
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
               FilmHub
             </span>
-          </a>
-          <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          </Link>
+
+          <div className="flex items-center md:order-2 space-x-4 md:space-x-2 rtl:space-x-reverse">
             <button
               type="button"
               className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-600"
@@ -29,14 +31,13 @@ function Header() {
             >
               <span className="sr-only">Open user menu</span>
               <img
-                className="w-8 h-8 rounded-full" // "class" замінено на "className"
+                className="w-8 h-8 rounded-full"
                 src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
                 alt="user photo"
               />
             </button>
-            {/* Dropdown menu */}
             <div
-              className="z-50 hidden my-4 text-base list-none bg-gray-700 divide-y divide-gray-600 rounded-lg shadow" // "class" замінено на "className"
+              className="z-50 hidden my-4 text-base list-none bg-gray-700 divide-y divide-gray-600 rounded-lg shadow"
               id="user-dropdown"
             >
               <div className="px-4 py-3">
@@ -47,22 +48,15 @@ function Header() {
               </div>
               <ul className="py-2" aria-labelledby="user-menu-button">
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="/my-tickets"
                     className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600 hover:text-white"
                   >
-                    Мій профіль
-                  </a>
+                    Мої квитки
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600 hover:text-white"
-                  >
-                    Налаштування
-                  </a>
-                </li>
-                <li>
+                  {/* TODO: Реалізувати логіку виходу */}
                   <a
                     href="#"
                     className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600 hover:text-white"
@@ -72,36 +66,34 @@ function Header() {
                 </li>
               </ul>
             </div>
+
+            <Link
+              to="/auth"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-3"
+            >
+              Увійти
+            </Link>
           </div>
+
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" // "class" замінено на "className"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-user"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-700 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-blue-500 rounded md:bg-transparent md:p-0"
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `block py-2 px-3 rounded md:bg-transparent md:p-0 ${
+                      isActive
+                        ? "text-blue-500"
+                        : "text-white hover:text-blue-500"
+                    }`
+                  }
                   aria-current="page"
                 >
                   Головна
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
-                >
-                  Зараз у кіно
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-500 md:p-0"
-                >
-                  Скоро у кіно
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
