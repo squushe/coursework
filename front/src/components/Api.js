7bb81b8d2d5e7cf3c763e4843c040b41df216ee9
squushe bc2025-5 <maksimrozko49@gmail.com>
2025-11-13 17:25:51 +0200

    Added new endpoint and changed path to login and register

--- front/src/components/Api.js
+++ front/src/components/Api.js
@@ -2,7 +2,7 @@ const API_BASE_URL = "http://localhost:3001/api";
 
 export const registerUser = async (userData) => {
   try {
-    const response = await fetch(`${API_BASE_URL}/register`, {
+    const response = await fetch(`${API_BASE_URL}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -25,7 +25,7 @@ export const registerUser = async (userData) => {
 
 export const loginUser = async (credentials) => {
   try {
-    const response = await fetch(`${API_BASE_URL}/login`, {
+    const response = await fetch(`${API_BASE_URL}/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -42,6 +42,21 @@ export const loginUser = async (credentials) => {
     return data;
   } catch (error) {
     console.error("API Error (loginUser):", error);
+    throw error;
+  }
+};
+export const getAllMovies = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/movies`);
+
+    if (!response.ok) {
+      throw new Error("Не вдалося завантажити фільми");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("API Error (getAllMovies):", error);
     throw error;
   }
 };
